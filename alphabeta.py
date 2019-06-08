a23525d9e11e14b8108906ade84672154f2988b4
“duniahakim” <doniahakim@gmail.com>
2019-06-08 11:18:12 -0700

    visualization

--- alphabeta.py
+++ alphabeta.py
@@ -92,37 +92,27 @@ def normalize(w):
 
 
 def getBlackWeights():
-    # w = {}
-    # w['num_black_Off_grid'] = -10.0
-    # w['num_white_Off_grid'] = 10.0
-    # w['num_black_on_edge'] = -1.0011062940155924
-    # w['num_white_on_edge'] =  0.9983901205525182
-    # w['black_average_pos'] = 0.9998278853441056
-    # w['white_average_pos'] = -1.0002396205464497
-    # w['black_coherence'] = 1.0000753562129148
-    # w['white_coherence'] = -0.9999787822015421
-    # w['black_break'] = -1.0
-    # w['white_break'] = 1.0
-    # return w
+    w = {'num_black_Off_grid': -10.0, 'num_white_Off_grid': 12.0, 'num_black_on_edge': -1.4220548966813016, 'num_white_on_edge': 1.0906714795992967, 'black_average_pos': -1.4156226804858798, 'white_average_pos': 1.3894678700263614, 'black_coherence': 1.7941345394011496, 'white_coherence': -0.8928301228450835, 'black_break': 1.849963784226139, 'white_break': -0.15003621577402654}
 
-    # w = {'num_white_Off_grid': 10.744423819269363, 'num_white_on_edge': 2.4513147103943327, 'black_break': 2.0006247871719154, 'num_black_Off_grid': -10.0, 'black_coherence': 2.1251995600701896, 'white_coherence': -0.26491617233354936, 'white_break': 0.2185629240155823, 'white_average_pos': 5.332047680479706, 'num_black_on_edge': 0.48002682171128863, 'black_average_pos': 1.845973466309996}
-    w = {'num_white_Off_grid': 12.619348456190396, 'num_white_on_edge': 3.1488994429705066, 'black_break': 2.857962452598955, 'num_black_Off_grid': -10.0, 'black_coherence': 2.8637717236477016, 'white_coherence': -0.4740966727316621, 'white_break': 1.042129028946902, 'white_average_pos': 3.918577000282171, 'num_black_on_edge': 0.33348825286889183, 'black_average_pos': 0.5794191606665251}
+    # w = {'num_white_Off_grid': 12.619348456190396, 'num_white_on_edge': 3.1488994429705066, 'black_break': 2.857962452598955, 'num_black_Off_grid': -10.0, 'black_coherence': 2.8637717236477016, 'white_coherence': -0.4740966727316621, 'white_break': 1.042129028946902, 'white_average_pos': 3.918577000282171, 'num_black_on_edge': 0.33348825286889183, 'black_average_pos': 0.5794191606665251}
     w = normalize(w)
     return w
 
 def getWhiteWeights():
-    w = {}
-    w['num_black_Off_grid'] = 100
-    w['num_white_Off_grid'] = -100
-    w['num_black_on_edge'] = 100
-    w['num_white_on_edge'] = -100
-    w['black_average_pos'] = 100
-    w['white_average_pos'] = -100
-    w['black_coherence'] = -100
-    w['white_coherence'] = 100
-    w['black_break'] = -100
-    w['white_break'] = 100
+    # w = {}
+    # w['num_black_Off_grid'] = 100
+    # w['num_white_Off_grid'] = -100
+    # w['num_black_on_edge'] = 100
+    # w['num_white_on_edge'] = -100
+    # w['black_average_pos'] = 100
+    # w['white_average_pos'] = -100
+    # w['black_coherence'] = -100
+    # w['white_coherence'] = 100
+    # w['black_break'] = -100
+    # w['white_break'] = 100
+    w = {'num_black_Off_grid': 11.622712460338024, 'num_white_Off_grid': -9.476337597924067, 'num_black_on_edge': 1.3089819093532071, 'num_white_on_edge': -0.18712317451057772, 'black_average_pos': 2.0062569948903897, 'white_average_pos': -0.061458291448646035, 'black_coherence': -0.006068921392942536, 'white_coherence': 1.85159872562587, 'black_break': 0.5038203759354308, 'white_break': 2.7659631373543543}
 
+    # w = {'num_black_Off_grid': 12.0, 'num_white_Off_grid': -9.838700530701312, 'num_black_on_edge': 2.0422668564407758, 'num_white_on_edge': -0.6614323172236901, 'black_average_pos': 2.272661061466584, 'white_average_pos': -0.5059960126085488, 'black_coherence': -0.5666191005121458, 'white_coherence': 2.1590475367578774, 'black_break': 0.19654327711591962, 'white_break': 2.243561057001353}
     w = normalize(w)
     return w
 
@@ -170,7 +160,20 @@ def getOrderedSuccStates(game, state, byIncreasingOrder):
     sortedPossibilities =  sorted(possibilities, key=lambda x: x[1], reverse = byIncreasingOrder)
     return list(zip(*sortedPossibilities))[0]
 
-
+def HeuristicAgent(game, state, side = 1):
+    choices = []
+    for action in game.actions(state):
+        succGameState = game.succ(state, action)
+        if succGameState is None:
+            continue
+        if side == game.black:
+            w = getBlackWeights()
+        elif side == game.white:
+            w = getWhiteWeights()
+        evaluation = (game.eval(succGameState, w), action)
+        choices.append(evaluation)
+    value, action = max(choices)
+    return action
 
 def AlphaBeta(game, state, side = 1):
 
@@ -245,10 +248,12 @@ def AlphaBeta(game, state, side = 1):
     value, action = max(choices)
     return action
 
-PercentageOfWinning = 0.0
-numGames = 10
+gamesWon = 0
+gamesLost = 0
+gamesDraw = 0
+numGames = 1
+averageNumMoves = 0.0
 for _ in range(numGames):
-    print("Num of Won Games So Far: ", PercentageOfWinning)
     game = model.AbaloneGame(100, boardSize = 3)
     policies = {game.black: AlphaBeta, game.white: humanPolicy}
     state = game.startState()
@@ -268,17 +273,23 @@ for _ in range(numGames):
         dict_pos, num_black_Off_grid, num_white_Off_grid, player, numRound = state
         print("Num Black Off Grid: ", num_black_Off_grid)
         print("Num White Off Grid: ", num_white_Off_grid)
-        print ("My program took", time.time() - start_time, "to run")
+        # print ("My program took", time.time() - start_time, "to run")
         game.visualization(dict_pos)
 
     dict_pos, num_black_Off_grid, num_white_Off_grid, player, numRound = state
+    averageNumMoves += numRound
     if (num_black_Off_grid >= game.numToWin):
         print("White Won!!")
+        gamesLost +=1
     elif (num_white_Off_grid >= game.numToWin):
         print("Black Won!!")
-        PercentageOfWinning += 1
+        gamesWon += 1
     else:
-        numGames -= 1
+        gamesDraw += 1
         print("No One Won!")
-PercentageOfWinning /= numGames
-print("Percentage of won games: ", PercentageOfWinning)
+
+averageNumMoves /= numGames
+print("Number of won games: ", gamesWon)
+print("Number of lost games: ", gamesLost)
+print("Number of draw games: ", gamesDraw)
+print("average Number of Moves: ", averageNumMoves)
--- tdlearning.py
+++ tdlearning.py
@@ -1,10 +1,41 @@
 import model
 
+def getBlackWeights():
+    w = {}
+    w['num_black_Off_grid'] = -10
+    w['num_white_Off_grid'] = 12
+    w['num_black_on_edge'] = -1
+    w['num_white_on_edge'] = 2
+    w['black_average_pos'] = -1
+    w['white_average_pos'] = 2
+    w['black_coherence'] = 2
+    w['white_coherence'] = -1
+    w['black_break'] = 2
+    w['white_break'] = 0
+    return w
+
+def getWhiteWeights():
+    w = {}
+    w['num_black_Off_grid'] = 12
+    w['num_white_Off_grid'] = -10
+    w['num_black_on_edge'] = 2
+    w['num_white_on_edge'] = -1
+    w['black_average_pos'] = 2
+    w['white_average_pos'] = -1
+    w['black_coherence'] = -1
+    w['white_coherence'] = 2
+    w['black_break'] = 0
+    w['white_break'] = 2
+    return w
+
+
+
 class TD:
-    def __init__(self, w, eta = 0.01, gamma = 0.2):
+    def __init__(self, w, game, eta = 0.00001, gamma = 0.2, side = 1):
         self.w = w #a dictionary
         self.gamma = gamma
         self.eta = eta
+        self.objective = side
 
     def target(self, game, nextState):
         r = game.utility(nextState)
@@ -35,7 +66,7 @@ class TD:
                 succStates.append(succ)
         dict_pos, num_black_Off_grid, num_white_Off_grid, player, numRound = state
         selectedState = None
-        if player == game.black:
+        if player == self.objective:
             selectedState = max(succStates, key = lambda k : game.eval(k, self.w))
         else:
             selectedState = min(succStates, key=lambda k: game.eval(k, self.w))
@@ -43,7 +74,7 @@ class TD:
         return selectedState
 
 
-w = {}
+# w = {}
 # w['num_black_Off_grid'] = -10
 # w['num_white_Off_grid'] = 10
 # w['num_black_on_edge'] = -1
@@ -55,20 +86,28 @@ w = {}
 # w['black_break'] = -1
 # w['white_break'] = 1
 
-w['num_black_Off_grid'] = -10
-w['num_white_Off_grid'] = 12
-w['num_black_on_edge'] = -1
-w['num_white_on_edge'] = 2
-w['black_average_pos'] = -1
-w['white_average_pos'] = 2
-w['black_coherence'] = 2
-w['white_coherence'] = -1
-w['black_break'] = 2
-w['white_break'] = 0
+# w['num_black_Off_grid'] = -10
+# w['num_white_Off_grid'] = 12
+# w['num_black_on_edge'] = -1
+# w['num_white_on_edge'] = 2
+# w['black_average_pos'] = -1
+# w['white_average_pos'] = 2
+# w['black_coherence'] = 2
+# w['white_coherence'] = -1
+# w['black_break'] = 2
+# w['white_break'] = 0
+
 game = model.AbaloneGame(500, boardSize = 2)
-td = TD(w)
-state = game.startState()
-while not game.isEnd(state):
-    state = td.iteration(game, state)
-    game.visualization(state[0])
-    print td.w
+side = game.white
+if side == game.black:
+    weights = getBlackWeights()
+elif side == game.white:
+    weights = getWhiteWeights()
+for i in range(100):
+    td = TD(weights, game, side = game.white)
+    state = game.startState()
+    while not game.isEnd(state):
+        state = td.iteration(game, state)
+        # game.visualization(state[0])
+        weights = td.w
+print(td.w)
